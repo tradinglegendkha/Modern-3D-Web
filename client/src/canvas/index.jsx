b8f91891879d5e50a22e781c7d2938fc056eb075
Buppi Khan <khanbuppi@gmail.com>
2023-08-09 14:01:14 -0400

    added other files

---
+++ client/src/canvas/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Canvas = () => {
+  return (
+    <div>
+      <h1>Canvas</h1>
+    </div>
+  );
+};
+
+export default Canvas;
---
+++ client/src/pages/Customizer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Customizer = () => {
+  return (
+    <div>
+      <h1>Customizer </h1>
+    </div>
+  );
+};
+
+export default Customizer;
