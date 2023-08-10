ca9e3781d9807541bb703740e50109c31e60f74e
Buppi Khan <khanbuppi@gmail.com>
2023-08-10 18:28:26 -0400

    added files for customization page

---
+++ client/src/components/AIPicker.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AIPicker = () => {
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  );
+};
+
+export default AIPicker;
---
+++ client/src/components/Tab.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tab = () => {
+  return (
+    <div>
+      <h1>Tab</h1>
+    </div>
+  );
+};
+
+export default Tab;
