3027b7b3412ee095a149b9a2b4919a6b3e9a9ef4
Buppi Khan <khanbuppi@gmail.com>
2023-08-14 14:06:27 -0400

    added camera angle position, backdrop or shadows behind the 3d shirt, and logo on the shirt

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
 //position of viewer camera
 
-const CameraRig = () => {
-  return <div>CameraRig</div>;
+const CameraRig = ({ children }) => {
+  const group = useRef();
+  const snap = useSnapshot(state);
+
+  useFrame((state, delta) => {
+    //for window sizes on computer and mobile
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+
+    //initial position of the model
+    let targetPosition = [-0.4, 0, 2];
+    if (snap.intro) {
+      if (isBreakpoint) targetPosition = [0, 0, 2];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
+    } else {
+      if (isMobile) targetPosition = [0, 0, 2.5];
+      else targetPosition = [0, 0, 2];
+    }
+
+    //set model camera position
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+
+    //model rotation smoothly
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    );
+  });
+
+  return <group ref={group}>{children}</group>;
 };
 
 export default CameraRig;
